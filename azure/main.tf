233129a429cd401be5a8aa97701cdd4c79da7678
Martin Luksik <martin.luksik@wireframe.digital>
2019-11-01 20:15:17 +0100

    rm provider

--- azure/main.tf
+++ azure/main.tf
@@ -1,8 +1,8 @@
 # Configure the Azure Provider
-provider "azurerm" {
-  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-  version = "=1.36.0"
-}
+#provider "azurerm" {
+#  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+#  version = "=1.36.0"
+#}
 
 # Create a resource group
 resource "azurerm_resource_group" "test" {
