de12708c7c17a74e7213bf0598fe564ebc823f1d
Martin Luksik <martin.luksik@wireframe.digital>
2019-11-01 23:12:53 +0100

    update record name set

--- azure/main.tf
+++ azure/main.tf
@@ -144,7 +144,7 @@ resource "azurerm_virtual_machine" "main" {
 resource "azurerm_dns_a_record" "main" {
   name                = "rancher"
   zone_name           = "mldedicated.net"
-  resource_group_name = "${azurerm_resource_group.main.name}"
+  resource_group_name = "mlstable"
   ttl                 = 1
   records             = ["${azurerm_public_ip.main.ip_address}"]
 }
