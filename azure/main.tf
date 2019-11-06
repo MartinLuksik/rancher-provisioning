285f7a31357020c937fc4ac96587d47e2d13ab07
Martin Luksik <martin.luksik@wireframe.digital>
2019-11-06 07:52:13 +0100

    output ip

--- azure/main.tf
+++ azure/main.tf
@@ -92,14 +92,13 @@ resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
   location            = "${azurerm_resource_group.main.location}"
   resource_group_name = "${azurerm_resource_group.main.name}"
-  network_security_group_id = azurerm_network_security_group.main.id
+  network_security_group_id = "${azurerm_network_security_group.main.id}"
 
   ip_configuration {
     name                          = "testconfiguration1"
     subnet_id                     = "${azurerm_subnet.internal.id}"
-    private_ip_address_allocation = "Dynamic"
+    private_ip_address_allocation = "Static"
     public_ip_address_id          = "${azurerm_public_ip.main.id}"
-
   }
 }
 
@@ -143,7 +142,11 @@ resource "azurerm_virtual_machine" "main" {
 
 data "azurerm_public_ip" "main" {
   name                = "${azurerm_public_ip.main.name}"
-  resource_group_name = "${azurerm_resource_group.main.name}"
+  resource_group_name = "${azurerm_virtual_machine.main.resource_group_name}"
+}
+
+output "public_ip_address" {
+  value = "${data.azurerm_public_ip.main.ip_address}"
 }
 
 resource "azurerm_dns_a_record" "main" {
