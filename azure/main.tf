2f5abba57b5a2d16e3cd092ffb67ba25f221e4d8
Martin Luksik <martin.luksik@wireframe.digital>
2019-11-06 07:55:34 +0100

    dynamic private ip

--- azure/main.tf
+++ azure/main.tf
@@ -97,7 +97,7 @@ resource "azurerm_network_interface" "main" {
   ip_configuration {
     name                          = "testconfiguration1"
     subnet_id                     = "${azurerm_subnet.internal.id}"
-    private_ip_address_allocation = "Static"
+    private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = "${azurerm_public_ip.main.id}"
   }
 }
